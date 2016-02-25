33d836b387bb928d416809d45ba9fd60121b6f28
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2016-02-25 09:30:21 -0300

    Edit & Delete task functionality added

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,8 @@
 import uuid from 'node-uuid';
 import React from 'react';
 
+import Notes from './Notes.jsx'
+
 export default class App extends React.Component {
   constructor(props){
     super(props);
@@ -31,16 +33,34 @@ export default class App extends React.Component {
     })
   }
 
+  editNote = (id, task) => {
+    if(!task.trim()){
+      return;
+    }
+
+    const notes = this.state.notes.map( note => {
+      if(note.id === id && task){
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
+  }
+
+  deleteNote = (id) => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    })
+  }
+
   render(){
     const notes = this.state.notes;
     return(
       <div>
-        <button onClick={this.addNote}>+</button>
-        <ul>
-          {notes.map(note =>
-            <li key={note.id}>{note.task}</li>
-          )}
-        </ul>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote}/>
       </div>
     );
   }
